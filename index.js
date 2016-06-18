22064fc7f252518f39561d930551fdf2f44ef136
apoterenko <a.poterenko@gmail.com>
2016-06-18 19:29:57 +0300

    *added build index files

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./src/angular2-dynamic-component'));
+//# sourceMappingURL=index.js.map
